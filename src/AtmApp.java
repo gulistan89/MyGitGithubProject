90cb1ef3467c6af096050f7a5637c9342905be16
Gülistan Güney <gulistangny3414@gmail.com>
2023-04-26 12:19:21 +0300

    v3

--- src/AtmApp.java
+++ src/AtmApp.java
@@ -4,5 +4,7 @@ public class AtmApp {
         //version1
         System.out.println("Java is easy");
         //version2
+        System.out.println("Learn Java earn money!!");
+        //version3
     }
 }
